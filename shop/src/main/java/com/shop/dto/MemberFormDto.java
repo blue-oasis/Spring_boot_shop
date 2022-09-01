19616cba011a3bc6c6e8cdc342283f2bd6b2311d
JiWon <radiohead@knou.ac.kr>
2022-09-01 15:57:31 +0900

    회원가입
    회원가입 구현
    javax.validation 적용 검사
    UserDetailsService 구현 회원가입

--- shop/src/main/java/com/shop/dto/MemberFormDto.java
+++ shop/src/main/java/com/shop/dto/MemberFormDto.java
@@ -2,18 +2,31 @@ package com.shop.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
-@Setter //회원 정보
+@Setter // 회원 정보
 public class MemberFormDto {
-    
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
 
+    @NotEmpty(message = "연락처는 필수 입력 값입니다.")
+    @Length(min = 11, max = 11, message = "연락처는 01012345678 형태로 입력해 주세요.")
     private String phone;
 }
--- shop/src/main/java/com/shop/service/MemberService.java
+++ shop/src/main/java/com/shop/service/MemberService.java
@@ -6,10 +6,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 @Service
 @Transactional // 로직 처리 중 에러 발생시 변경된 데이터를 로직수행 이전 상태로 콜백
 @RequiredArgsConstructor // final @NonNull 필드에 생성자 추가
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -23,6 +28,21 @@ public class MemberService {
         if (findMember != null) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
 
+    @Override //오버라이딩 email을 파라미터로 받음
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder() //User 객체 반환
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
+        
     }
 }
