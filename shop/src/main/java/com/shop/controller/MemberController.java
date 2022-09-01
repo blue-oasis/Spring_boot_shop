10f5baf7acc10fe513434a9b41f66787441182de
JiWon <radiohead@knou.ac.kr>
2022-09-01 13:25:52 +0900

    회원가입
    회원가입 컨트롤러
    회원가입 폼 페이지 (각 맞춰야함)

--- shop/src/main/java/com/shop/controller/MemberController.java
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -8,17 +8,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.shop.entity.Member;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @RequestMapping("/members")
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
-    public String memberForm(Model model) {
+    public String memberForm(Model model) { //회원 가입 페이지로 이동
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String memberForm(MemberFormDto memberFormDto) {
+
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+
+        return "redirect:/";
     }
 
 }
