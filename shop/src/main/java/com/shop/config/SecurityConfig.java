6210324bd058ba8c1c87177e57979695d65bfc66
JiWon <radiohead@knou.ac.kr>
2022-09-01 15:57:55 +0900

    로그인 구현
    로그인 기능, 페이지 구현

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -8,17 +8,41 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 @Configuration
 @EnableWebSecurity //WebSecurityConfigurerAdapter 상속 클래스에 선언하면 SpringSecurityFilterChain 자동 포함
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MemberService memberService;
     
     @Override //http 요청에 대한 보안 설정
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.formLogin()
+            .loginPage("/members/login") //로그인 페이지 url
+            .defaultSuccessUrl("/") //로그인 성공시 이동
+            .usernameParameter("email") // 로그인 시 사용할 파라미터
+            .failureUrl("/members/login/error") //로그인 실패시 이동
+            .and()
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) //로그아웃 url
+            .logoutSuccessUrl("/"); //로그아웃 성공시 이동
     }
 
     @Bean // 해시함수 이용하여 비밀번호 암호화
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+        throws Exception {  //인증 AuthenticationManagerBuilder가 AuthenticationManager 생성
+            auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
+            //userDetailsService 구현객체 지정/ 비밀번호 암호화 passwordEncoder
+        }
+    
 }
--- shop/src/main/java/com/shop/controller/MemberController.java
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -12,6 +12,9 @@ import com.shop.entity.Member;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+
 @RequestMapping("/members")
 @Controller
 @RequiredArgsConstructor
@@ -21,18 +24,37 @@ public class MemberController {
     private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
-    public String memberForm(Model model) { //회원 가입 페이지로 이동
+    public String memberForm(Model model) { // 회원 가입 페이지로 이동
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
 
-    @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto memberFormDto) {
+    @PostMapping(value = "/new") //검증하려는 객체 앞에 @Valid 선언, 파라미터로 bindingResult추가
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult,
+                            Model model) {
+        if(bindingResult.hasErrors()) {
+            return "member/memberForm"; //에러가 있으면 회원가입 페이지로 이동
+        }
 
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) { //중복가입 에러 발생시 에러메시지 전달
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
 
+    @GetMapping(value = "/login")
+    public String loginMember() {
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
+    }
 }
