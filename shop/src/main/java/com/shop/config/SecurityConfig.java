07ecf49cc57726c879dd6d43af08e3c83f745f9b
JiWon <radiohead@knou.ac.kr>
2022-09-07 16:14:56 +0900

    로그인 오류
    부트스트랩 적용안되는거 픽스
    로그인 왜 안댐
    405오류
    등등 난리남

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     MemberService memberService;
 
-    @Override // http 요청에 대한 보안 설정
+  /*  @Override // http 요청에 대한 보안 설정
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .loginPage("/members/login") // 로그인 페이지 url
@@ -33,6 +33,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) // 로그아웃 url
                 .logoutSuccessUrl("/"); // 로그아웃 성공시 이동
+
+        http.authorizeRequests()
+                .mvcMatchers("/", "/members/**",
+                        "/item/**", "/images/**").permitAll() // 모든 사용자가 인증없이 접속가능하도록 설정
+                .mvcMatchers("/admin/**").hasRole("ADMIN") // /admin으로 시작하는 경로는 ADMIN역할만 접근가능
+                .anyRequest().authenticated(); // 지정한 경로 제외 모두 인증요구 설정
+
+        http.exceptionHandling() // 인증되지 않은 사용자 접근 시 수행되는 핸들러
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+    } */
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/")
+        ;
+
+        http.authorizeRequests()
+                .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        ;
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+        ;
     }
 
     @Bean // 해시함수 이용하여 비밀번호 암호화
@@ -47,33 +81,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // userDetailsService 구현객체 지정/ 비밀번호 암호화 passwordEncoder
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .loginPage("/members/login")
-                .defaultSuccessUrl("/")
-                .usernameParameter("email")
-                .failureUrl("/members/login/error")
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
-                .logoutSuccessUrl("/");
-
-        http.authorizeRequests()
-                .mvcMatchers("/", "members/**",
-                        "/item/**", "/images/**")
-                .permitAll() // 모든 사용자가 인증없이 접속가능하도록 설정
-                .mvcMatchers("/admin/**").hasRole("ADMIN") // /admin으로 시작하는 경로는 ADMIN역할만 접근가능
-                .anyRequest().authenticated(); // 지정한 경로 제외 모두 인증요구 설정
-
-        http.exceptionHandling() // 인증되지 않은 사용자 접근 시 수행되는 핸들러
-                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
-
-    }
-
-    @Override // static 디렉토리 하위 파일은 인증무시
+    @Override // static 디렉토리 하위 파일은 인증무시 + 부트스트랩도..나중에 css폴더에 넣을까
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/**");
     }
 
 }
--- shop/src/main/java/com/shop/controller/MemberController.java
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.shop.entity.Member;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,17 +30,17 @@ public class MemberController {
         return "member/memberForm";
     }
 
-    @PostMapping(value = "/new") //검증하려는 객체 앞에 @Valid 선언, 파라미터로 bindingResult추가
+    @PostMapping(value = "/new") // 검증하려는 객체 앞에 @Valid 선언, 파라미터로 bindingResult추가
     public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult,
-                            Model model) {
-        if(bindingResult.hasErrors()) {
-            return "member/memberForm"; //에러가 있으면 회원가입 페이지로 이동
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm"; // 에러가 있으면 회원가입 페이지로 이동
         }
 
         try {
             Member member = Member.createMember(memberFormDto, passwordEncoder);
             memberService.saveMember(member);
-        } catch (IllegalStateException e) { //중복가입 에러 발생시 에러메시지 전달
+        } catch (IllegalStateException e) { // 중복가입 에러 발생시 에러메시지 전달
             model.addAttribute("errorMessage", e.getMessage());
             return "member/memberForm";
         }
@@ -47,14 +48,16 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @GetMapping(value = "/login")
+    //@GetMapping(value = "/login")
+    //@PostMapping(value = "/login")
+    @RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
     public String loginMember() {
-        return "/member/memberLoginForm";
+        return "member/memberLoginForm";
     }
 
     @GetMapping(value = "/login/error")
     public String loginError(Model model) {
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
-        return "/member/memberLoginForm";
+        return "member/memberLoginForm";
     }
 }
