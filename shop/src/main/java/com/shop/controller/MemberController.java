e0ec122066c48bcaf602af118b09dd2178444e33
JiWon <radiohead@knou.ac.kr>
2022-09-28 10:52:09 +0900

    로그인 관련 에러
    계속 로그인 성공이 안됨..예시코드 받아서 하는건 돌아감 원인을 모르겠음

--- shop/src/main/java/com/shop/controller/MemberController.java
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -48,16 +48,14 @@ public class MemberController {
         return "redirect:/";
     }
 
-    //@GetMapping(value = "/login")
-    //@PostMapping(value = "/login")
-    @RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
+    @GetMapping(value = "/login")
     public String loginMember() {
-        return "member/memberLoginForm";
+        return "/member/memberLoginForm";
     }
 
     @GetMapping(value = "/login/error")
     public String loginError(Model model) {
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
-        return "member/memberLoginForm";
+        return "/member/memberLoginForm";
     }
 }
